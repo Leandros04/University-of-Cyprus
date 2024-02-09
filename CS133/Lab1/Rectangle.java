df005703423effa509176b12ee92bae7eed1aa23
Leandros <148828146+Leandros04@users.noreply.github.com>
2024-02-09 13:37:51 +0200

    Add files via upload

---
+++ CS133/Lab1/Rectangle.java
@@ -0,0 +1,19 @@
+package cs.lab1;
+
+public class Rectangle 
+{
+	public Rectangle(int p1, int p2)
+	{
+		
+	}
+	
+	public Rectangle(int p, int s1, int s2)
+	{
+		
+	}
+	
+	public Rectangle(int p1, int p2, int s1, int s2)
+	{
+		
+	}
+}
---
+++ CS133/Lab1/TestRationalNumbers.java
@@ -0,0 +1,11 @@
+package cs.lab1;
+
+public class TestRationalNumbers {
+
+	public static void main(String[] args) {
+		RationalNumbers rational = new RationalNumbers();
+		
+		
+	}
+
+}
